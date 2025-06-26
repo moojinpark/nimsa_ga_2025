c9a94291cd8b10d9d3712d98c65bf07bec087013
moojinmoojinpark <adamuahmadgulma@gmail.com>
2025-06-26 09:37:35 +0100

    Fix: Update STATIC_URL and reorganize STATICFILES_DIRS in settings.py

--- nimsa_ga_2025/settings.py
+++ nimsa_ga_2025/settings.py
@@ -114,14 +114,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mainapp', 'static')]
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mainapp/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
